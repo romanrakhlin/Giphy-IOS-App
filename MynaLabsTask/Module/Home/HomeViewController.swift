c2c8de5dbf51bdfca1bc94cceaae5d85651ff56a
Roman Rakhlin <rrakhlin@gmail.com>
2022-07-26 22:22:08 +0300

    fix: little bit t of refactor

--- MynaLabsTask/Module/Home/HomeViewController.swift
+++ MynaLabsTask/Module/Home/HomeViewController.swift
@@ -176,7 +176,9 @@ extension HomeViewController: UIScrollViewDelegate {
     }
 }
 
-extension HomeViewController: GiphyLayoutDelegate {
+// MARK: - MosaicLayoutDelegate
+extension HomeViewController: MosaicLayoutDelegate {
+    
     func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
         guard let downSized = GIFs[indexPath.row].images?.downsized,
             let width = downSized.width, let height = downSized.height else {
@@ -189,74 +191,5 @@ extension HomeViewController: GiphyLayoutDelegate {
         let ratio = collectionViewHalfWidth / CGFloat(downloadedImageWidth)
         
         return downloadedImageHeight * ratio
-    }
-}
-
-protocol GiphyLayoutDelegate: AnyObject {
-    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat
-}
-
-class GiphyLayout: UICollectionViewLayout {
-    
-    weak var delegate: GiphyLayoutDelegate?
-    private let numberOfColumns = 2
-    private let cellPadding: CGFloat = 3
-    private var cache: [UICollectionViewLayoutAttributes] = []
-    private var contentHeight: CGFloat = 0
-    
-    private var contentWidth: CGFloat {
-        guard let collectionView = collectionView else {
-            return 0
-        }
-        
-        let insets = collectionView.contentInset
-        return collectionView.bounds.width - (insets.left + insets.right)
-    }
-    
-    override var collectionViewContentSize: CGSize {
-        return CGSize(width: contentWidth, height: contentHeight)
-    }
-    
-    override func prepare() {
-        guard let collectionView = collectionView else {
-            return
-        }
-        
-        cache.removeAll()
-        let columnWidth = contentWidth / CGFloat(numberOfColumns)
-        var xOffset: [CGFloat] = []
-        for column in 0..<numberOfColumns {
-            xOffset.append(CGFloat(column) * columnWidth)
-        }
-        
-        var column = 0
-        var yOffset: [CGFloat] = .init(repeating: 0, count: numberOfColumns)
-        
-        for item in 0..<collectionView.numberOfItems(inSection: 0) {
-            let indexPath = IndexPath(item: item, section: 0)
-            
-            let photoHeight = delegate?.collectionView(collectionView, heightForPhotoAtIndexPath: indexPath) ?? 180
-            let frame = CGRect(x: xOffset[column], y: yOffset[column], width: columnWidth, height: photoHeight)
-            let insetFrame = frame.insetBy(dx: cellPadding, dy: cellPadding)
-            
-            let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-            attributes.frame = insetFrame
-            cache.append(attributes)
-            
-            contentHeight = frame.maxY
-            yOffset[column] = yOffset[column] + photoHeight
-            
-            column = column < (numberOfColumns - 1) ? (column + 1) : 0
-        }
-    }
-    
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        var visibleLayoutAttributes: [UICollectionViewLayoutAttributes] = []
-        for attributes in cache {
-            if attributes.frame.intersects(rect) {
-                visibleLayoutAttributes.append(attributes)
-            }
-        }
-        return visibleLayoutAttributes
     }
 }
